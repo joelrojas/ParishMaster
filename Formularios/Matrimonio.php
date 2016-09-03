e76c00cb8b9e4a564c0057141a7f25395f246d65
DESKTOP-T5D7VJM\Adri <flash.ar79@gmail.com>
2016-09-03 18:11:46 -0400

    add class fiel form matrimonio segun mockup sin funcionalidad completa

--- Formularios/Matrimonio.php
+++ Formularios/Matrimonio.php
@@ -9,63 +9,192 @@
 	<!-- Bootstrap css -->
 	<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
 
-	<title>Matrimonio</title>
+	<title>Registro Matrimonio</title>
 </head>
 <body>
 	<?php
 		require_once '../clases/parroquia.php';
-		require_once '../clases/sacerdote.php'
+		require_once '../clases/sacerdote.php';
+		require_once '../clases/fiel.php';
 	?>
 	<div class="container">
 		<div class="page-header">
-		  <h1>Matrimonio</h1>
+		  <h1>Registro Matrimonio</h1>
 		</div>
 		<form>
-			<div class="form-group">
-				<label for="nombre">Nombre Esposo:</label>
-				<input type="text" class="form-control" id="nombre">
-			</div>
-			<div class="form-group">
-				<label for="apellido">Apellido Esposo:</label>
-				<input type="text" class="form-control" id="apellido">
-			</div>
-			<div class="form-group">
-				<label for="nombre">Nombre Esposa:</label>
-				<input type="text" class="form-control" id="nombre">
-			</div>
-			<div class="form-group">
-				<label for="apellido">Apellido Esposa:</label>
-				<input type="text" class="form-control" id="apellido">
-			</div>
-			<div class="form-group">
-				<label for="parroquia">Parroquia:</label>
-				<select class="form-control" name="parroquia">
-					<option value="">Escoja una parroquia</option>
-					<?php
-					$parr= new parroquia(1,"aa");
-					$parrs=$parr->GetAll();
-					while($fila=mysql_fetch_array($parrs)){
-						echo "<option value='".$fila['idParroquia']."'>".$fila['Nombre']."</option>";
-					}
-					?>
-				</select>
+			<div class = "panel panel-default">
+				<div class = "panel-heading">
+     				<h3 class = "panel-title">Datos Parroquia</h3>
+     			</div>
+     			<div class = "panel-body">
+     				<div class="form-group">
+						<label for="parroquia">Iglesia Parroquial:</label>
+						<select class="form-control" name="parroquia">
+							<option value="">Escoja una Iglesia Parroquial</option>
+							<?php
+							//$parr= new parroquia(1,"aa");
+							//$parrs=$parr->GetAll();
+							//while($fila=mysql_fetch_array($parrs)){
+							//	echo "<option value='".$fila['idParroquia']."'>".$fila['Nombre']."</option>";
+							//}
+							?>
+						</select>
+					</div>
+
+					<div class="form-group">
+						<label for="presbitero">Presbitero:</label>
+						<select class="form-control" name="presbitero">
+							<option value="">Escoja un Presbitero</option>
+							<?php
+							//$sac= new sacerdote();
+							//$sacs=$sac->GetAll();
+							//while($fila=mysql_fetch_array($sacs)){
+							//	echo "<option value='".$fila['idSacerdote']."'>".$fila['Nombre']." ".$fila['Apellido']."</option>";
+							//}
+							?>
+						</select>
+					</div>
+     			</div>
+     		</div>
+
+			<div class = "panel panel-default">
+				<div class = "panel-heading">
+					<h3 class = "panel-title">Datos Esposos</h3>
+				</div>
+				<div class = "panel-body">
+					<div class="form-group">
+						<?php 
+							if(isset($_GET['esposa']))
+							{
+								$esposa=new fiel();
+								$esposa=fiel::withID($_GET['esposa']);
+								if($esposa->nombre=="ERROR")
+								{
+									echo "<label for='esposa'>ID Esposa:</label>
+									  <div class = 'input-group'>
+											<input type='number' class='form-control' id='esposa'>
+											<span class = 'input-group-btn'>
+												<button class = 'btn btn-info' type = 'button'>Verificar</button>
+											</span>
+									  </div>";
+									echo "<div class='alert alert-danger'><strong>Error!</strong> Este ID es incorrecto.</div>";
+								}
+								else
+								{
+									echo "<label for='esposa'>ID Esposa:</label>
+									  <div class = 'input-group'>
+											<input type='number' class='form-control' id='esposa' value='".$_GET['esposa']."'>
+											<span class = 'input-group-btn'>
+												<button class = 'btn btn-info' type = 'button'>Verificar</button>
+											</span>
+									  </div>";
+									echo "<label for='esposa'>Nombre Esposa:</label>
+										  <input type='text' class='form-control' id='esposa' value='".$esposa->nombre."' readonly>";
+									echo "<label for='esposa'>Apellido Esposa:</label>
+										  <input type='text' class='form-control' id='esposa' value='".$esposa->apellido."' readonly>";	
+								}
+								
+									  
+							}
+							else 
+								echo "<label for='esposa'>ID Esposa:</label>
+									  <div class = 'input-group'>
+											<input type='number' class='form-control' id='esposa'>
+											<span class = 'input-group-btn'>
+												<button class = 'btn btn-info' type = 'button'>Verificar</button>
+											</span>
+									  </div>";
+						?>
+						
+					</div>
+					<div class="form-group">
+						<?php 
+							if(isset($_GET['esposo']))
+							{
+								$esposo=new fiel();
+								$esposo=fiel::withID($_GET['esposo']);
+								if($esposo->nombre=="ERROR")
+								{
+									echo "<label for='esposo'>ID Esposo:</label>
+									  <div class = 'input-group'>
+											<input type='number' class='form-control' id='esposo'>
+											<span class = 'input-group-btn'>
+												<button class = 'btn btn-info' type = 'button'>Verificar</button>
+											</span>
+									  </div>";
+									echo "<div class='alert alert-danger'><strong>Error!</strong> Este ID es incorrecto.</div>";
+								}
+								else
+								{
+									echo "<label for='esposo'>ID Esposo:</label>
+									  <div class = 'input-group'>
+											<input type='number' class='form-control' id='esposo' value='".$_GET['esposo']."'>
+											<span class = 'input-group-btn'>
+												<button class = 'btn btn-info' type = 'button'>Verificar</button>
+											</span>
+									  </div>";
+									echo "<label for='esposo'>Nombre Esposo:</label>
+										  <input type='text' class='form-control' id='esposa' value='".$esposo->nombre."' readonly>";
+									echo "<label for='esposo'>Apellido Esposo:</label>
+										  <input type='text' class='form-control' id='esposa' value='".$esposo->apellido."' readonly>";	
+								}
+								
+									  
+							}
+							else 
+								echo "<label for='esposa'>ID Esposo:</label>
+									  <div class = 'input-group'>
+											<input type='number' class='form-control' id='esposo'>
+											<span class = 'input-group-btn'>
+												<button class = 'btn btn-info' type = 'button'>Verificar</button>
+											</span>
+									  </div>";
+						?>
+					</div>
+					<div class="form-group">
+						<label for="fecha">Fecha Matrimonio:</label>
+						<input type="date" class="form-control" id="fecha">
+					</div>
+				</div>
 			</div>
 
-			<div class="form-group">
-				<label for="sacerdote">Sacerdote:</label>
-				<select class="form-control" name="sacerdote">
-					<option value="">Escoja un sacerdote</option>
-					<?php
-					$sac= new sacerdote();
-					$sacs=$sac->GetAll();
-					while($fila=mysql_fetch_array($sacs)){
-						echo "<option value='".$fila['idSacerdote']."'>".$fila['Nombre']." ".$fila['Apellido']."</option>";
-					}
-					?>
-				</select>
+			<div class = "panel panel-default">
+				<div class = "panel-heading">
+					<h3 class = "panel-title">Datos Testigos</h3>
+				</div>
+				<div class = "panel-body">
+					<div class="form-group">
+						<label for="padrino">ID Padrino:</label>
+						<input type="text" class="form-control" id="padrino">
+						<label for="padrino">ID Padrino:</label>
+						<input type="text" class="form-control" id="padrino">
+						<label for="padrino">ID Padrino:</label>
+						<input type="text" class="form-control" id="padrino">
+						<label for="padrino">ID Padrino:</label>
+						<input type="text" class="form-control" id="padrino">
+					</div>
+					
+				</div>
 			</div>
-				
-			<button type="submit" class="btn btn-default">Registrar</button>
+
+			<div class = "panel panel-default">
+				<div class = "panel-heading">
+					<h3 class = "panel-title">Datos Registro Civil</h3>
+				</div>
+				<div class = "panel-body">
+					<div class="form-group">
+						<label for="oficialia">Oficialia del Registo Civil:</label>
+						<input type="text" class="form-control" id="oficialia">
+						<label for="partido">Partido:</label>
+						<input type="text" class="form-control" id="partido">
+						<label for="numero">Numero:</label>
+						<input type="text" class="form-control" id="numero">
+					</div>
+					
+				</div>
+			</div>
+
+			<button type="submit" class="btn btn-success btn-lg btn-block">Registrar</button>
 		</form>
 
 
