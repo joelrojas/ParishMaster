04aaf4da4a2cf20d197ff4f208e2471242cb5866
DESKTOP-T5D7VJM\Adri <flash.ar79@gmail.com>
2016-09-18 20:30:41 -0400

    listado de request listo, sin enviar correos aun

---
+++ Formularios/listadorequest.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+	<!-- Bootstrap css -->
+	<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+
+	<title>Request</title>
+</head>
+<body>
+	<?php
+		require_once "../general/headersac.php";
+
+		require_once '../clases/request.php';
+		$_SESSION['idPersona']=5;
+		$idPersona=$_SESSION['idPersona'];
+	?>
+		
+	<div class="container">
+		<div class="page-header">
+		  <h1>Request</h1>
+		</div> 		
+ 			<div class = "panel panel-default">
+				<div class = "panel-heading">
+					<h3 class = "panel-title">Solicitudes Pendientes</h3>
+				</div>
+				<div class = "panel-body">
+					<table class="table table-hover">
+    					<thead>
+    						<tr>
+        						<th>Nombre Fiel</th>
+        						<th>Sacramento</th>
+        						<th>Parroquia</th>
+        						<th>Seleccionar</th>
+        					</tr>
+    					</thead>
+    					<tbody>
+    						<form action="reqespeciales.php">
+    						<?php
+    								$req=new request();
+                                    $reqs=$req->get_reqs();
+                                    while($fila=DatabaseHandler::fetchrow($reqs))
+                                    {
+                                    	echo "<tr>";
+                                        echo "<td>".$fila['Nombre']." ".$fila['Apellido']."</td>";
+                                        echo "<td>".$fila['sacramento']."</td>";
+                                        echo "<td>".$fila['parroquia']."</td>";
+                                        echo "<td><button type='submit' class='btn btn-success' id='idreq' name='idreq' value='".$fila['idRequest']."' >Atender</button></td>";
+                                        echo "</tr>";
+                                    }
+    						?>
+    						</form>
+    					</tbody>
+    				</table>
+				</div>
+			</div>
+	</div>
+
+	<!-- jQuery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+	<!-- Bootstrap javascript -->
+	<script src="../bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+</html>
--- clases/request.php
+++ clases/request.php
@@ -41,4 +41,68 @@ class request
         return $res;
     }
 
+    public function get_reqs()
+    {
+        $q="SELECT request.idRequest, persona.Nombre, persona.Apellido, sacramento.Nombre as sacramento, request.mensaje, request.libro, request.pagina, request.numero, parroquia.Nombre as parroquia
+            FROM request, persona, sacramento, parroquia 
+            WHERE request.idPersona=persona.idPersona
+            AND request.idSacramento=sacramento.idSacramento
+            AND request.idParroquia=parroquia.idParroquia
+            AND atendida=0";
+        $res=$this->dbh->exequery($q);
+        if ($this->dbh->mysqli->error)
+        {
+            printf("Errormessaged: %s\n", $this->dbh->mysqli->error);
+        }
+        return $res;
+    }
+
+    public function get_req($id)
+    {
+        $q="SELECT request.idRequest, persona.Nombre, persona.idPersona, persona.Apellido, sacramento.Nombre as sacramento, sacramento.idSacramento, request.mensaje, request.libro, request.pagina, request.numero, parroquia.Nombre as parroquia
+            FROM request, persona, sacramento, parroquia 
+            WHERE request.idPersona=persona.idPersona
+            AND request.idSacramento=sacramento.idSacramento
+            AND request.idParroquia=parroquia.idParroquia
+            AND atendida=0
+            AND request.idRequest=".$id;
+        $res=$this->dbh->exequery($q);
+        if ($this->dbh->mysqli->error)
+        {
+            printf("Errormessaged: %s\n", $this->dbh->mysqli->error);
+        }
+        $res=$this->dbh->fetchrow($res);
+        return $res;
+    }
+
+    public function isespecial($id)
+    {
+        $q="SELECT libro FROM request WHERE idRequest=".$id;
+        $res=$this->dbh->exequery($q);
+        if ($this->dbh->mysqli->error)
+        {
+            printf("Errormessaged: %s\n", $this->dbh->mysqli->error);
+        }
+        $res=$this->dbh->fetchrow($res);
+       // if(is_null($res['libro']))
+        return $res['libro'];
+    }
+
+    public function set_respuesta($id,$msj)
+    {
+        $q="UPDATE request SET respuesta='".$msj."', atendida=1 WHERE idRequest=".$id;
+        $res=$this->dbh->exequery($q);
+        if ($this->dbh->mysqli->error)
+        {
+            printf("Errormessaged: %s\n", $this->dbh->mysqli->error);
+        }
+        
+    }
+
+    
+
 }
+
+//$a=new request();
+//if(empty($a->isespecial($_GET['idreq']))) echo"null--".$a->isespecial($_GET['idreq']);
+//else echo "not null--".$a->isespecial($_GET['idreq']);
