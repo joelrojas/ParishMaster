8def8167188726421471de0a930f12ffd7075a88
Pamela Figueroa <i.pfp94@gmail.com>
2016-09-28 17:22:54 -0400

    changed header sacerdote

--- Principal/principal.php
+++ Principal/principal.php
@@ -8,57 +8,11 @@
   </head>
 <body>
 
-    <div class="navbar navbar-default navbar-static-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#"><span>ParishMaster</span></a>
-        </div>
-        <div class="collapse navbar-collapse" id="navbar-ex-collapse">
-          <ul class="nav navbar-nav navbar-right">
-            <li class="">
-              <a href="principal.php">Inicio</a>
-            </li>
-            <li class="">
-              <a href="../Formularios/mandadoremails.php">Enviar Mails</a>
-            </li>
-            <li class="">
-              <a href="../Formularios/busqueda.php">Buscar Certificado</a>
-            </li>
-              <li class="">
-                  <a href="../Formularios/sacerdotereg.php">Registrar Sacerdote</a>
-              </li>
-            <li class="dropdown">
-              <ul class="dropdown-menu" role="menu">
-                <li>
-                  <a href="../Formularios/NacimientoReg.php">Bautizo</a>
-                </li>
-                <li>
-                  <a href="../Formularios/ComunionReg.php">Comunion</a>
-                </li>
-                <li>
-                  <a href="../Formularios/Confirmacion.php">Confirmacion</a>
-                </li>
-                                <li>
-                  <a href="../Formularios/Matrimonio.php">Matrimonio</a>
-                </li>
-                
-                
-                
-                
-                
-              </ul>
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Registro Canonico <i class="fa fa-caret-down"></i></a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+<?php 
+
+require_once "../general/headersac.php"
+
+?>
     <div class="section">
       <div class="container">
         <div class="row">
